cdb6ba39f7acef3f6c99f81df12e41dbb1b55e80
ellemonke <ellemonke@gmail.com>
2020-03-23 15:34:15 -0700

    map scaled

--- static/js/map.js
+++ static/js/map.js
@@ -1,23 +1,25 @@
+google.charts.load('current', {
+  'packages': ['geochart'],
+  'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+});
+
 $.ajax({
   url: "static/resources/Happiness_2019.csv",
   async: true,
-  success: function (csvd) {
-    indicator = $.csv.toArrays(csvd);
+  success: function (csvData) {
+    indicator = $.csv.toArrays(csvData, { onParseValue: $.csv.hooks.castToScalar });
   },
   dataType: "text",
   complete: function () {
-
-    google.charts.load('current', {
-      'packages': ['geochart'],
-      'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-    });
     
     google.charts.setOnLoadCallback(drawRegionsMap);
     
     function drawRegionsMap() {
       var data = google.visualization.arrayToDataTable(indicator);
     
-      var options = {};
+      var options = {
+        colorAxis: {colors: ['white', 'gold']},
+      };
     
       var chart = new google.visualization.GeoChart(document.getElementById('map_div'));
     
